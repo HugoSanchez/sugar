f4a1d58da1e63198456b8819cf46b1bf71a9d0ff
HugoSanchez <hugosanchez251@gmail.com>
2024-10-04 13:56:51 +0100

    fixed menu

--- src/components/SlidingMenuLayout.tsx
+++ src/components/SlidingMenuLayout.tsx
@@ -38,24 +38,23 @@ const SlidingMenuLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="relative min-h-screen overflow-hidden">
 			{/* Stationary menu (always present, underneath) */}
-			<div className="fixed inset-y-0 left-0 w-64 bg-white text-black z-0">
-				<nav className="p-4 pt-20">
-					<ul className="space-y-2">
-						<li><a href="#" className="block p-2 hover:bg-gray-700 rounded">Read</a></li>
-						<li><a href="#" className="block p-2 hover:bg-gray-700 rounded">Write</a></li>
-						<li><a href="#" className="block p-2 hover:bg-gray-700 rounded">Profile</a></li>
+			<div
+				className={`fixed z-20 inset-y-0 left-0 h-screen w-64 bg-gray-50 opacity-90 text-black border-r border-gray-200 transform transition-transform duration-300 ease-in-out ${
+					isMenuOpen ? 'translate-x-0' : '-translate-x-full'
+				}`}
+			>
+				<nav className="p-4 md:pl-14 pt-20">
+					<ul className="space-y-4">
+						<li><a href="/read" className="block p-2 rounded hover:italic">read.</a></li>
+						<li><a href="/write" className="block p-2 rounded hover:italic">write.</a></li>
+						<li><a href="#" className="block p-2 rounded hover:italic">profile.</a></li>
+						<li><a href="#" className="block p-2 rounded hover:italic absolute bottom-10">logout.</a></li>
 					</ul>
 				</nav>
 			</div>
 
-			{/* Sliding main content (including header) */}
-			<div
-				className={`fixed inset-0 bg-gray-50 transition-transform duration-300 border-l border-gray-200 ease-in-out ${
-					isMenuOpen ? 'translate-x-64' : 'translate-x-0'
-				}`}
-				onClick={closeMenu}
-			>
-				{/* Header */}
+			{/* Header */}
+			<div onClick={closeMenu}>
 				<header className={`z-10 fixed top-0 left-0 right-0 bg-gray-50 transition-shadow duration-300 h-16 ${
 					isScrolled ? 'shadow-md' : ''
 				}`}>
