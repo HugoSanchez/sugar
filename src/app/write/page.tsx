4844ad02a95255f4a6d0ff69be2f1d24bfb78c25
HugoSanchez <hugosanchez251@gmail.com>
2025-02-25 10:35:48 +0100

    getting clean

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -17,7 +17,7 @@ export default function Write() {
 
 	return (
 		<>
-			<div className='flex flex-col py-20 md:py-32 px-8 md:px-80 '>
+			<div className='flex flex-col py-10 md:py-32 px-2 md:px-80 '>
 				<RevervEditor onEditorChange={handleEditorChange}/>
 				<PostButton editorContent={editorContent}/>
 			</div>
